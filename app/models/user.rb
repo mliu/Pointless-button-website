090068ced2de9216e3777e596de365a3be9b81c9
Michael <michael.liu@id.me>
2013-07-29 16:45:31 -0400

    Login

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  validates_uniqueness_of :username, before: create
+  validates_uniqueness_of :email, before: create
+  validates_presence_of :username, :email, :password, before: create
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 LoginTest::Application.routes.draw do
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  root 'users#new'
+
+  match "secrets" => "secrets#index", :via => :get
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
