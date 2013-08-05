7d69e9c2b9b6093e3a38cbab558c2c5fba32f7cf
Michael <michael.liu@id.me>
2013-08-05 15:40:59 -0400

    Deleting sqlite...

---
+++ vendor/bundle/ruby/2.0.0/gems/json-1.8.0/ext/json/ext/parser/siteconf20130805-4097-u9n29v.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130805-4097-sqq5fn"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
