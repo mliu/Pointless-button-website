53f99d2774e7b85fa4d020359d923fee4f301ab4
Michael <michael.liu@id.me>
2013-08-07 11:54:01 -0400

    Fixing deploy error

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20130807142704) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  # enable_extension "plpgsql"
 
   create_table "clicks", force: true do |t|
     t.integer  "user_id"
